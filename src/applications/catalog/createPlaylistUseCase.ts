8b84751a59df3481479122c38166cb131cbaa964
Rodrigo de Souza <rsouza01@gmail.com>
2020-10-23 23:17:45 +0200

    Catalog use cases added

---
+++ src/applications/catalog/createPlaylistUseCase.ts
@@ -0,0 +1,7 @@
+import UseCase from "../infrastructure/useCase";
+
+export default class CreatePlaylistUseCase extends UseCase {
+  async execute(): Promise<any> {
+      return {};
+  }
+}
---
+++ src/applications/catalog/deleteAlbumUseCase.ts
@@ -0,0 +1,7 @@
+import UseCase from "../infrastructure/useCase";
+
+export default class DeleteAlbumUseCase extends UseCase {
+  async execute(): Promise<any> {
+      return {};
+  }
+}
